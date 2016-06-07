57348ec16cfb58c2019e2a218da5dbf9378fddd1
CS20160602 <zhangwenze@lanou3g.com>
2016-06-07 17:44:50 +0800

    add a project

---
+++ JustTestGit/JustTestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JustTestGit
+//
+//  Created by wenze on 16/6/7.
+//  Copyright © 2016年 wenze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JustTestGit/JustTestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JustTestGit
+//
+//  Created by wenze on 16/6/7.
+//  Copyright © 2016年 wenze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
